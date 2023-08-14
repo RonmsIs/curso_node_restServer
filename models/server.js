1b3ff3dd48e7d63a7db453fe9b83acfbe5a4f9fe
Ismael <ronaldo.molina.salas@gmail.com>
2023-08-14 12:25:01 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors');
+class Server{
+
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT;
+        this.usuariosRoutePath = '/api/usuarios'
+        //Middlewares funcion que siempre se levanta al iniciar el servidor
+        this.middlewares();
+        //rutas de mi aplicación
+        this.routes();  
+    }
+
+    middlewares(){
+        //CORS
+        this.app.use(cors());
+        //Parseio y lecura del body
+        this.app.use(express.json());
+        //directorio publico
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+        this.app.use(this.usuariosRoutePath, require('../routes/usuarios'));
+    }
+
+    listen(){
+        this.app.listen(this.port,() => {
+            console.log('',this.port)
+        });
+        
+    }
+}
+
+module.exports = Server;
