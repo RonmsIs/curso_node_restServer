16154d5f382ab5c0bfd4e1bebe1009cc042bdb17
Ronaldo Molina <ronaldo.molina.salas@gmail.com>
2023-08-14 13:00:12 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ class Server{
 
     constructor(){
         this.app = express()
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 8000;
         this.usuariosRoutePath = '/api/usuarios'
         //Middlewares funcion que siempre se levanta al iniciar el servidor
         this.middlewares();
